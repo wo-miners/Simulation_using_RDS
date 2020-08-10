12950e8cf45c6ff2885ab8422244d02c6a15027b
wo-miners <68655924+wo-miners@users.noreply.github.com>
2020-08-10 21:53:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ var app = new Vue({
         cameraViewer: null,
     },
     // helper methods to connect to ROS
-    methods: {
+       methods: {
         connect: function () {
             this.loading = true
             this.ros = new ROSLIB.Ros({
@@ -29,4 +29,77 @@ var app = new Vue({
             this.ros.on('error', (error) => {
                 this.logs.unshift((new Date()).toTimeString() + ` - Error: ${error}`)
             })
-            this.ros.on('close', (
+            this.ros.on('close', () => {
+                this.logs.unshift((new Date()).toTimeString() + ' - Disconnected!')
+                this.connected = false
+                this.loading = false
+            })
+        },
+        disconnect: function () {
+            this.ros.close()
+        },
+        setCamera: function () {
+            console.log('set camera method')
+            this.cameraViewer = new MJPEGCANVAS.Viewer({
+                divID: 'mjpeg',
+                host: '18.181.196.205',
+                width: 640,
+                height: 480,
+                topic: '/camera/rgb/image_raw',
+                port: 11315,
+            })
+        },
+        setTopic: function () {
+            this.topic = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/cmd_vel',
+                messageType: 'geometry_msgs/Twist'
+            })
+        },
+        forward: function () {
+            this.message = new ROSLIB.Message({
+
+                linear: { x: 1, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: 0, },
+            })
+            this.setTopic()
+            this.topic.publish(this.message)
+        },
+        stop: function () {
+            this.message = new ROSLIB.Message({
+
+                linear: { x: 0, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: 0, },
+            })
+            this.setTopic()
+            this.topic.publish(this.message)
+        },
+        backward: function () {
+            this.message = new ROSLIB.Message({
+
+                linear: { x: -1, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: 0, },
+            })
+            this.setTopic()
+            this.topic.publish(this.message)
+        },
+        turnLeft: function () {
+            this.message = new ROSLIB.Message({
+                linear: { x: 0.5, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: 0.5, },
+            })
+            this.setTopic()
+            this.topic.publish(this.message)
+        },
+        turnRight: function () {
+            this.message = new ROSLIB.Message({
+                linear: { x: 0.5, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: -0.5, },
+            })
+            this.setTopic()
+            this.topic.publish(this.message)
+        },
+    },
+    mounted() {
+    },
+})
